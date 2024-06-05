233001f719993a6fc0ea573486bac755b97d29e4
Yair Andrey Rodriguez Navarro <yarodriuezn@ufpso.edu.co>
2024-06-05 15:25:47 -0500

    Petición Get

---
+++ FastAPI/metodo_get.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+# Uso BaseModel para crear un modelo (ENTIDAD) de usuario a trabjar
+app = FastAPI()
+
+# Entidad user
+class User(BaseModel):
+    name : str
+    age : int
+    id : int
+
+    
+
+users_list = [User(id = 1,name="Yair", age=19),
+         User(id = 2, name="Jorge",age=29),
+         User(id = 3,name="Carlos",age=39),
+         ]
+
+
+
+@app.get("/usersjson")
+async def get_users():
+    return [
+        {"name" : "Yair", "age":19},
+        {"name" : "Jorge", "age":29},
+        {"name" : "Carlos", "age":39},
+    ]
+
+@app.get("/users")
+async def users_class():
+    return users_list
+
+# Usando el Path
+@app.get("/users/{id}")
+async def user(id:int):
+    search_user(id)
+    
+
+# Con Query
+@app.get("/usersquery/")
+async def user(id:int):
+    search_user(id)
+
+# Función para buscar usuario
+def search_user(id:int):
+    users = filter(lambda user : user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except :
+        return "{'error': 'No se encuentra el usuario'}"
